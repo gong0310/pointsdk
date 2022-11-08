05b5d3da0e9e5c2e0851eb327e119baf90016a8a
gong0310 <2691733570@qq.com>
2022-11-08 22:00:20 +0800

    style: 添加注释

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,6 +2,7 @@ import { DefaultOptons, TrackerConfig, Options } from "../types/index";
 export default class Tacker {
   public data: Options;
   constructor(options: Options) {
+    // 参数合并
     this.data = Object.assign(this.initDef, options);
   }
   // 初始化默认参数
